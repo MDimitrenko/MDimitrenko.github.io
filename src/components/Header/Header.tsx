1097dab7ed1aa361118de612fc9a0f63f59584c5
VysokikhAnna <anutka320@gmail.com>
2023-07-03 16:38:13 +0300

    компоненты Header, Layout, Logo на ts

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,33 @@
+import React, { useRef, useLayoutEffect } from "react";
+import './Header.css';
+import Logo from '../Logo/Logo';
+
+type HeaderProps = {
+    children?: React.ReactElement;
+};
+
+const Header = ({children}: HeaderProps) => {
+    const stickyHeader = useRef(null);
+
+    useLayoutEffect(() => {
+        const header = document.getElementById("header");
+        let fixedTop = stickyHeader.current.offsetTop;
+        const stickyHeaderEvent = () => {
+            if (window.scrollY > fixedTop) {
+                header.classList.add("sticky-header");
+            } else {
+                header.classList.remove("sticky-header");
+            }
+        };
+        window.addEventListener("scroll", stickyHeaderEvent);
+    }, []);
+
+    return (
+        <div className='header' id='header' ref={stickyHeader}>
+            <Logo/>
+            {children}
+        </div>
+    )
+}
+
+export default Header
