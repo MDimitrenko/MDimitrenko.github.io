8a610494f6a29f0a08892fdf3bcd563da4c385a6
VysokikhAnna <anutka320@gmail.com>
2023-09-07 09:52:52 +0300

    Поправлены стили

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import s from './Layout.module.sass';
 import Header from '../Header/Header';
-import { useTranslation } from 'react-i18next';
 
 type LayoutProps = {
   children?: React.ReactNode;
 };
 
 const Layout = ({ children }: LayoutProps) => {
-  const { t } = useTranslation();
   return (
-    <>
-      <div>
-        <Header />
-        <main>{children}</main>
-      </div>
-    </>
+    <div className={s.layout}>
+      <Header />
+      <main>{children}</main>
+    </div>
   );
 };
 
